e99128409d65773750c20538db58e0637b402cf5
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-19 16:36:48 +0600

    applying common function

--- js/login.js
+++ js/login.js
@@ -4,21 +4,21 @@ function getUserInput(inputFieldId) {
     return inputField.value;
 }
 
-
-document.getElementById('login-button').addEventListener('click', function () {
-    // get user email
-    const userEmail = getUserInput('user-email');
-    // const emailField = document.getElementById('user-email');
-    // const userEmail = emailField.value;
-    // get user password
-    const passwordField = document.getElementById('user-password');
-    const userPassword = passwordField.value;
-
-    //auth
+/*******authenticaion*******/
+function auth(userEmail, userPassword) {
     const errorMessage = document.getElementById('error-message');
     if (userEmail == 'sajidmahamud835@gmail.com' && userPassword == '123456') {
         window.location.href = 'dashboard.html';
     } else {
         errorMessage.innerText = "Username or password was wrong.";
     }
+}
+
+document.getElementById('login-button').addEventListener('click', function () {
+    // get user email
+    const userEmail = getUserInput('user-email');
+    // get user password
+    const userPassword = getUserInput('user-password');
+    //auth
+    auth(userEmail, userPassword);
 })
