2ed7fd697bfa7b4ed9117e7e2d4f978a6f7cd2b1
sajidmahamud835 <sajidmahamud835@gmail.com>
2021-08-19 16:30:19 +0600

    creating common function for getting user input

--- js/login.js
+++ js/login.js
@@ -1,7 +1,15 @@
+/*******get user input******/
+function getUserInput(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    return inputField.value;
+}
+
+
 document.getElementById('login-button').addEventListener('click', function () {
     // get user email
-    const emailField = document.getElementById('user-email');
-    const userEmail = emailField.value;
+    const userEmail = getUserInput('user-email');
+    // const emailField = document.getElementById('user-email');
+    // const userEmail = emailField.value;
     // get user password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
